09b53d5b897e8bf58623034212e1178fb40e4eb7
Camila Porfírio <camila.porfirio@gmail.com>
2022-08-22 14:49:22 +0200

    adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css';
-import { ThemeProvider } from '@emotion/react';
-import { Typography } from '@mui/material';
-import theme from './theme/Theme';
+import React from "react";
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { Typography } from "@mui/material";
+import theme from "./theme/Theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Typography variant='h2'>The SafeSpace Solution;</Typography>
-      <Typography variant='body1'>Our goal is not to serve as a self diagnosis or replace the need of searching for professional help, but to raise awareness on the theme and encourage people to seek help if needed.</Typography>
-      <Typography variant='subtitle1'>Our goal is not to serve as a self diagnosis or replace the need of searching for professional help, but to raise awareness on the theme and encourage people to seek help if needed.</Typography>
-      <Typography variant='subtitle2'>Our goal is not to serve as a self diagnosis or replace the need of searching for professional help, but to raise awareness on the theme and encourage people to seek help if needed.</Typography>
-      <Typography variant='button'>Our goal is not to serve as a self diagnosis or replace the need of searching for professional help, but to raise awareness on the theme and encourage people to seek help if needed.</Typography>
+      <Typography variant="h2">The SafeSpace Solution;</Typography>
+      <Typography variant="caption">
+        Our goal is not to serve as a self diagnosis or replace the need of
+        searching for professional help, but to raise awareness on the theme and
+        encourage people to seek help if needed.
+      </Typography>
+      <Typography variant="body1">
+        Our goal is not to serve as a self diagnosis or replace the need of
+        searching for professional help, but to raise awareness on the theme and
+        encourage people to seek help if needed.
+      </Typography>
+      <Typography variant="subtitle1">
+        Our goal is not to serve as a self diagnosis or replace the need of
+        searching for professional help, but to raise awareness on the theme and
+        encourage people to seek help if needed.
+      </Typography>
+      <Typography variant="subtitle2">
+        Our goal is not to serve as a self diagnosis or replace the need of
+        searching for professional help, but to raise awareness on the theme and
+        encourage people to seek help if needed.
+      </Typography>
+      <Typography variant="button">
+        Our goal is not to serve as a self diagnosis or replace the need of
+        searching for professional help, but to raise awareness on the theme and
+        encourage people to seek help if needed.
+      </Typography>
     </ThemeProvider>
   );
 }
--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -1,10 +1,9 @@
 import { createTheme } from "@mui/system";
-import '@fontsource/nunito/300.css';
-import '@fontsource/nunito/400.css';
-import '@fontsource/nunito/600.css';
-import '@fontsource/nunito/600.css';
+import "@fontsource/nunito/300.css";
+import "@fontsource/nunito/400.css";
+import "@fontsource/nunito/600.css";
+import "@fontsource/nunito/600.css";
 import "@fontsource/croissant-one";
-
 
 const theme = createTheme({
   typography: {
@@ -12,24 +11,84 @@ const theme = createTheme({
       fontFamily: "Croissant One",
       letterSpacing: "-0.08em",
       fontWeight: "400",
+      fontSize: "1.375em",
+      lineHeight: "1.5em",
+    },
+    h3: {
+      fontFamily: "Poppins",
+      fontWeight: "400",
+      fontSize: "1.5em",
+      lineHeight: "auto",
     },
     subtitle1: {
-      fontFamily: "Nunito",
-    },
-    subtitle2: {
-      fontFamily: "Nunito",
-    },
-    button: {
-      fontFamily: "Nunito",
+      fontFamily: "Poppins",
+      letterSpacing: "0.00938em",
+      fontWeight: "400",
+      fontSize: "0.875em",
+      lineHeight: "1.5em",
     },
     body1: {
-      fontSize: "1.25em",
-      fontFamily: "Nunito",
+      fontFamily: "Poppins",
+      fontSize: "1em",
+      fontWeight: "400",
+      lineHeight: "auto",
+    },
+    caption: {
+      fontFamily: "Poppins",
+      fontSize: "0.75em",
+      fontWeight: "400",
+      lineHeight: "1.5em",
+      color: "rgba(7, 33, 57, 0.38)",
+    },
+    subtitle2: {
+      fontFamily: "Poppins",
+      letterSpacing: "0.00714em",
+      fontWeight: "500",
+      fontSize: "0.75em",
+      lineHeight: "1.125em",
+    },
+    button: {
+      fontFamily: "Poppins",
+      letterSpacing: "0.06em",
       fontWeight: "600",
-      lineHeight: "33px",
-      color: "rgba(7, 33, 57, 0.6)"
+      fontSize: "0.75em",
+      lineHeight: "1.25em",
     },
   },
-})
+  palette: {
+    commom: {
+      black: "#072139",
+      white: "#FFF",
+    },
+    primary: {
+      main: "#4D5CB5",
+    },
+    secondary: {
+      main: "#C5C5F8",
+    },
+    error: {
+      main: "#FE7745",
+    },
+    warning: {
+      main: "#FFCE6D",
+    },
+    info: {
+      main: "#4D5CB5",
+    },
+    success: {
+      main: "#C4DC63",
+    },
+    text: {
+      primary: "#072139",
+      secondary: "rgba(7, 33, 57, 0.6)",
+      disabled: "rgba(7, 33, 57, 0.38)",
+    },
+    divider: "rgba(7, 33, 57, 0.12)",
+    background: {
+      paper: "#FFF",
+      default: "#F5F6FA",
+    },
+  },
+});
 
 export default theme;
